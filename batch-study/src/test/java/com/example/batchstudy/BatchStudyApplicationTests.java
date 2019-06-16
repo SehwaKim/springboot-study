25156a4ee8fb00ec49f66d5dce74c0b40b6b981a
sehwa <sehwakim@gmail.com>
2019-06-16 23:55:52 +0900

    spring batch example

---
+++ batch-study/src/test/java/com/example/batchstudy/BatchStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.batchstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatchStudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
